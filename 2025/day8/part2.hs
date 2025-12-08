88ded7ec51ca192892787fb33ec3ddfbf8c69ea8
ggp <georgii.galushko@gmail.com>
2025-12-09 02:20:42 +0300

    completed day 8

---
+++ 2025/day8/part2.hs
@@ -0,0 +1,38 @@
+import Input
+
+type Point = (Integer, Integer, Integer)
+
+sort :: (a -> a -> Bool) -> [a] -> [a]
+sort _ [] = []
+sort le (x:xs) = sort le (filter (not . le x) xs) ++ x:sort le (filter (le x) xs)
+
+dist :: Point -> Point -> Integer
+dist (x1, y1, z1) (x2, y2, z2) = (x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2
+
+dists :: [Point] -> [(Integer, (Int, Int))]
+dists points =
+  let go [] = []
+      go ((i, p):ps) = [(dist p p', (i, j)) | (j, p') <- ps] ++ go ps
+   in go (zip [0,1..] points)
+
+connect :: Eq a => [[a]] -> (a, a) -> [[a]]
+connect sets (x, y) =
+  let go [] [] [] = [[x, y]]
+      go [] [] [yset] = [x:yset]
+      go [] [xset] [] = [y:xset]
+      go [] hasx hasy = [(concat hasx) ++ (concat hasy)]
+      go (s:sets) hasx hasy
+        | x `elem` s && y `elem` s = s:sets
+        | x `elem` s = go sets (s:hasx) hasy
+        | y `elem` s = go sets hasx (s:hasy)
+        | otherwise = s:go sets hasx hasy
+   in go sets [] []
+
+main :: IO ()
+main =
+  let go [s] _ (i, j) | length s == length boxes =
+        let (x1, _, _) = boxes!!i
+            (x2, _, _) = boxes!!j
+         in x1 * x2
+      go sets (idx:ds) _ = go (connect sets idx) ds idx
+   in print $ go [] (map snd $ sort (<) (dists boxes)) (0, 0)
